ff9885db507203769ca4df64591b9bfc0ad06ef4
AnikiFan <15800788661@163.com>
2024-11-25 00:32:48 +0800

    更改auth相关文件组织方式

--- src/ui/dashboard/sidenav.tsx
+++ src/ui/dashboard/sidenav.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 import NavLinks from '@/ui/dashboard/nav-links';
 import AcmeLogo from '@/ui/acme-logo';
 import { PowerIcon } from '@heroicons/react/24/outline';
-import { signOut } from '@/auth';
+import { signOut } from '@/auth/auth';
 export default function SideNav() {
   return (
     <div className="flex h-full flex-col px-3 py-4 md:px-2">
